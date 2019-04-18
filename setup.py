f0cfa2f732c84917071390dfaee8596e018907ac
Maciej Gębarski <mgebarski@gmail.com>
2019-04-18 20:11:23 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='proxprovider',
+    version='0.1',
+    description='Manage many proxy providers from one place',
+    # url='http://github.com/storborg/funniest',
+    author='Maciej Gębarski',
+    author_email='mgebarski@gmail.com',
+    license='MIT',
+    packages=['proxprovider'],
+    zip_safe=False
+)
