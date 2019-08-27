df47d03f7ef4f210d5f492d058cfdf47141dc1b3
Maciej Gębarski <mgebarski@gmail.com>
2019-08-27 23:40:17 +0200

    Add provider for https://free-proxy-list.net/

---
+++ proxprovider/providers/free_proxy_list_net.py
@@ -0,0 +1,41 @@
+import requests
+import bs4
+
+from . import ProxProviderModelBase
+
+
+class FreeProxyListNet(ProxProviderModelBase):
+    """
+    Provider for https://free-proxy-list.net/.
+    Additional parameters:
+        timeout: http timeout (default: 2s), only_https return
+        only https proxy
+    """
+    def proxies(self, only_https=False, timeout=2):
+        out = []
+        soup = bs4.BeautifulSoup(
+            requests.get(
+                "https://free-proxy-list.net/", timeout=timeout
+            ).text,
+            'html.parser'
+        )
+        trs = soup.find('table', attrs={'id': 'proxylisttable'}).find_all('tr')
+        for i, tr in enumerate(trs):
+            if i == 0:
+                # Table header
+                continue
+            if len(tr) != 8:
+                continue
+
+            if tr.find('th'):
+                continue
+
+            tds = tr.find_all('td')
+            if only_https and tds[6].text == 'no':
+                continue
+
+            out.append("http://{host}:{port}".format(
+                host=tds[0].text, port=tds[1].text
+            ))
+
+        return out
